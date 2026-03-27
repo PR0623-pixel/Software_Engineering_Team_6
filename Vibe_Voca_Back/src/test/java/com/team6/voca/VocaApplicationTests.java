c3958a6631709b2c366fa2bfb4c0cbc96236ae31
Hyeonseok Jeong <thema00000@naver.com>
2026-03-27 20:34:56 +0900

    초기 세팅

---
+++ Vibe_Voca_Back/src/test/java/com/team6/voca/VocaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.team6.voca;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VocaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
