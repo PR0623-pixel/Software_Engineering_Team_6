17a536d20e8500f8177a7f1bf52fc7d89ef375c5
PR0623-pixel <fksfksfn623@gmail.com>
2026-05-02 15:35:12 +0900

    김홍석 - 임시 API 저장브랜치

---
+++ Vibe_Voca_Back/src/main/java/com/team6/voca/controller/WordController.java
@@ -0,0 +1,5 @@
+package com.team6.voca.controller;
+
+public class WordController {
+    
+}
--- Vibe_Voca_Front/vite.config.js
+++ Vibe_Voca_Front/vite.config.js
@@ -10,9 +10,20 @@ export default defineConfig({
     vue(),
     vueDevTools(),
   ],
-  resolve: {
-    alias: {
-      '@': fileURLToPath(new URL('./src', import.meta.url))
-    },
-  },
+  server: {
+    proxy: {
+      '/api': {
+        target: 'http://localhost:8080',
+        changeOrigin: true
+      }
+    }
+  }
 })
+
+//   },
+//   resolve: {
+//     alias: {
+//       '@': fileURLToPath(new URL('./src', import.meta.url))
+//     },
+//   },
+// })
