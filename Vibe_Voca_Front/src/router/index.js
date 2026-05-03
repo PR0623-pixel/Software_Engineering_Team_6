cd70660809ff0b785e89200297e01b794d1eee1e
Hyeonseok Jeong <thema00000@naver.com>
2026-05-03 16:55:05 +0900

    정현석 - 단어장 페이지 만듬

--- Vibe_Voca_Front/src/router/index.js
+++ Vibe_Voca_Front/src/router/index.js
@@ -4,12 +4,14 @@ import HomeView from '../views/HomeView.vue'
 import LoginView from '../views/LoginView.vue'
 import RegisterView from '../views/RegisterView.vue'
 import MainView from '../views/MainView.vue'
+import WordsView from '../views/WordsView.vue'
 
 const routes = [
   { path: '/',         name: 'home',     component: HomeView },
   { path: '/login',    name: 'login',    component: LoginView },
   { path: '/register', name: 'register', component: RegisterView },
   { path: '/main',     name: 'main',     component: MainView },
+  { path: '/words',    name: 'words',    component: WordsView },
 ]
 
 const router = createRouter({
